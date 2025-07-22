b8a693641a585400959f3492eedabc56a6a45983
Santosh Kumar <santoshcse97@gmail.com>
2025-07-22 10:22:27 +0530

    added solution for 1071 and 1768

---
+++ python_solution/greatest_common_divisor_of_strings.py
@@ -0,0 +1,45 @@
+from time_it import time_it
+
+def greatest_common_divisor_of_strings(str1: str, str2: str) -> str:   
+    if len(str2) >len(str1):
+        return greatest_common_divisor_of_strings(str2, str1)
+    if str1 == str2:
+        return str1
+    if str1.startswith(str2):
+        return greatest_common_divisor_of_strings(str1[len(str2):], str2)
+    return ""
+
+
+if __name__ == "__main__":
+    words = [
+        ["ABCABC", "ABC"],
+        ["ABABAB", "ABAB"],
+        ["LEET", "CODE"],
+        ["abcd", "pq"],
+        ["", ""],
+    ]
+    for word in words:
+        print(f'GCD of {word[0]} and {word[1]}:  {greatest_common_divisor_of_strings(word[0], word[1])}\n')
+
+"""
+1071. Greatest Common Divisor of Strings
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example 1:
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Example 2:
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+Constraints:
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+"""
---
+++ python_solution/time_it.py
@@ -0,0 +1,23 @@
+import time
+from functools import wraps
+
+def time_it(func): # This is the decorator function
+    """
+    Decorator that measures the execution time of a function.
+    """
+    @wraps(func)  # Preserve the original function's metadata
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # Record the start time
+        result = func(*args, **kwargs)   # Call the original function
+        end_time = time.perf_counter()    # Record the end time
+        execution_time = end_time - start_time  # Calculate the elapsed time
+
+        # print time taken in ms
+        # print(f"Function '{func.__name__}' took {execution_time:.12f} seconds to execute.")
+
+        # print time taken in ms
+        ms = execution_time*1000
+        print(f"Function '{func.__name__}' took {ms:.12f} ms to execute.")
+
+        return result    
+    return wrapper
