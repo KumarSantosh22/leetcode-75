ac77d457479b401c7ee089d0275d662cd68c770a
Santosh Kumar <santoshcse97@gmail.com>
2025-08-10 21:07:54 +0530

    added solution for #1679

--- python_solution/time_it.py
+++ python_solution/time_it.py
@@ -6,19 +6,23 @@ def time_it(func):  # This is the decorator function
     """
     Decorator that measures the execution time of a function.
     """
-    @wraps(func)  # Preserve the original function's metadata
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()  # Record the start time
-        result = func(*args, **kwargs)   # Call the original function
-        end_time = time.perf_counter()    # Record the end time
-        execution_time = end_time - start_time  # Calculate the elapsed time
+    try:
+        @wraps(func)  # Preserve the original function's metadata
+        def wrapper(*args, **kwargs):
+            start_time = time.perf_counter()  # Record the start time
+            result = func(*args, **kwargs)   # Call the original function
+            end_time = time.perf_counter()    # Record the end time
+            execution_time = end_time - start_time  # Calculate the elapsed time
 
-        # print time taken in ms
-        # print(f"Function '{func.__name__}' took {execution_time:.12f} seconds to execute.")
+            # print time taken in ms
+            # print(f"Function '{func.__name__}' took {execution_time:.12f} seconds to execute.")
 
-        # print time taken in ms
-        ms = execution_time*1000
-        print(f"Function '{func.__name__}' took \033[35m{ms:.12f}\033[0m ms to execute.")
+            # print time taken in ms
+            ms = execution_time*1000
+            print(f"Function '{func.__name__}' took \033[35m{ms:.12f}\033[0m ms to execute.")
 
-        return result
-    return wrapper
+            return result
+        return wrapper
+    
+    except Exception as ex:
+        print(f"\033[31m{ex}\033[0m")
